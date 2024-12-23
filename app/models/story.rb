a41b5524f59120319eb8162eab00a713788a865a
Kadeem Jackson <kdjackson35@gmail.com>
2024-12-23 15:29:31 -0500

    Add story model validations, and associations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,17 @@
 class Story < ApplicationRecord
+  validates :title, :body, :author_id, presence: true
+  validates :title, length: { maximum: 255 }
+  validates :title, uniqueness: true
+
+  belongs_to :author,
+             primary_key: :id,
+             foreign_key: :author_id,
+             class_name: 'User',
+             inverse_of: :stories
+
+  has_many :responses,
+           primary_key: :id,
+           foreign_key: :story_id,
+           class_name: 'Response',
+           inverse_of: :story
 end
