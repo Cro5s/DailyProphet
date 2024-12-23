d6688a0f606d39669cf021dbfd2515529dcea857
Kadeem Jackson <kdjackson35@gmail.com>
2024-12-23 14:40:49 -0500

    Add validations, relationships, and methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,56 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, :session_token, :password_digest, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+  has_many :stories,
+           primary_key: :id,
+           foreign_key: :author_id,
+           class_name: 'Story',
+           dependent: :destroy
+
+  has_many :responses,
+           primary_key: :id,
+           foreign_key: :author_id,
+           class_name: 'Response',
+           dependent: :destroy
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_credentials_by(username, password)
+    user = User.find_by(username: username)
+    return user if user&.valid_password?(password)
+
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+
+    session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
