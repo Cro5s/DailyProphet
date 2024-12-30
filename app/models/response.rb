6107b5cf807468eab2a92618048282ab6a24e720
Kadeem Jackson <kdjackson35@gmail.com>
2024-12-30 13:49:38 -0500

    Add Response model validations and associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,15 @@
 class Response < ApplicationRecord
+  validates :body, :author_id, :story_id, presence: true
+
+  belongs_to :author,
+             primary_key: :id,
+             foreign_key: :author_id,
+             class_name: 'User',
+             inverse_of: :responses
+
+  belongs_to :story,
+             primary_key: :id,
+             foreign_key: :story_id,
+             class_name: 'Story',
+             inverse_of: :responses
 end
