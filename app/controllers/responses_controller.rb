b4c48152af121cbd9cdea61d3958cf8bcf09034d
Kadeem Jackson <kdjackson35@gmail.com>
2025-01-01 18:56:30 -0500

    Add ResponsesController

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,39 @@
+class ResponsesController < ApplicationController
+  before_action :ensure_logged_in
+
+  def index
+    @responses = Response.includes(params[:story_id]).all
+
+    if @responses.any?
+      render :index
+    else
+      render json: { responses: [] }
+    end
+  end
+
+  def create
+    @response = Response.new(response_params)
+
+    @response.author_id = Current.user.id
+
+    if @response.save
+      render :show
+    else
+      render json: @response.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @response = Response.find(params[:id])
+    @response.destroy
+    @responses = Response.includes(:author).all
+
+    render :show
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:body, :story_id, :author_id)
+  end
+end
